72f699fe310a4c05fc04e64f0ed6adcf75466e2d
Bishnu Pandey <bishnu@gksil.com>
2014-04-02 10:48:09 +0530

    Project skeleton commited

---
+++ UT/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UT::Application.config.secret_key_base = 'e1b9fb508eafb520fdc2ed018c09cf9e7e0d836b16ef57a84c4195f95cd59450554d8327573454382c06c7845ac1b5f31559d23f28ddceaf2490b2e875243213'
